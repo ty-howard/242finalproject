12acaca142154cb115d70b72017be91a777c8111
Ty <trhoward@email.sc.edu>
2024-04-18 02:32:50 -0400

    updated final project

--- public/scripts/data.js
+++ public/scripts/data.js
@@ -8,37 +8,29 @@ const getPlanes = async() => {
 
 const showPlanes = async() => {
   const planes = await getPlanes();
-  const planesDiv = document.getElementById("resultscont");
+  const planesDiv = document.getElementById("gallery");
   planesDiv.innerHTML = "";
 
   planes.forEach((plane)=>{
-
-    document.getElementById("resultscont").append(getPlaneSection(plane));
-
-  });
-};
-  
-  const getPlaneSection = (plane) => {
     const result = document.createElement("section");
     result.classList.add("one");
     result.setAttribute("id","result");
-    result.onclick = () => openModal(plane);
   
     const columns = document.createElement("div");
     columns.classList.add("columns");
     result.append(columns);
   
     const imgSection = document.createElement("section");
-    imgSection.classList.add("one");
+    imgSection.classList.add("three");
     imgSection.setAttribute("id","resimg");
     columns.append(imgSection);
   
     const img = document.createElement("img");
-    img.src = "./images/" + plane.main_image;
+    img.src = "./images/" + plane.img;
     imgSection.append(img);
   
     const txtSection = document.createElement("section");
-    txtSection.classList.add("one");
+    txtSection.classList.add("two");
     txtSection.setAttribute("id","restxt");
     columns.append(txtSection);
   
@@ -62,54 +54,205 @@ const showPlanes = async() => {
     result.append(hr);
   
     const h4 = document.createElement("h4");
-    h4.innerHTML = "Logged on " + plane.date + " by " + plane.user;
+    h4.innerHTML = "Logged on <b>" + plane.date + "</b> by <b>" + plane.user + "</b";
+    h4.classList.add("center");
     result.append(h4);
-  
-    return result;
+
+    planesDiv.append(result);
+
+      result.onclick = (e) => {
+          e.preventDefault();
+          displayDetails(plane);
+      };
+  });
+};
+
+const displayDetails = (plane) => {
+  openDialog("plane-details");
+
+  const planeDetails = document.getElementById("plane-details");
+  planeDetails.classList.add("columns");
+  planeDetails.innerHTML = "";
+
+  const dataColumn = document.createElement("div");
+  dataColumn.classList.add("one");
+
+  const dLink = document.createElement("a");
+  dLink.innerHTML = "	&#9249;";
+  dataColumn.append(dLink);
+  dLink.id = "delete-link";
+
+  const eLink = document.createElement("a");
+  eLink.innerHTML = "&#9998;";
+  dataColumn.append(eLink);
+  eLink.id = "edit-link";
+
+  const al = document.createElement("p");
+  al.innerHTML = "Airline: " + plane.airline;
+  dataColumn.append(al);
+
+  const ac = document.createElement("p");
+  ac.innerHTML = "Aircraft: " + plane.aircraft;
+  dataColumn.append(ac);
+
+  const ali = document.createElement("p");
+  ali.innerHTML = "Alliance: " + plane.alliance;
+  dataColumn.append(ali);
+
+  const reg = document.createElement("p");
+  reg.innerHTML = "Registration: " + plane.registration;
+  dataColumn.append(reg);
+
+  const date = document.createElement("p");
+  date.innerHTML = "Seen at RDU on " + plane.date + " by " + plane.user;
+  dataColumn.append(date);
+
+  const hr = document.createElement("hr");
+  hr.classList.add("detHR");
+  dataColumn.append(hr);
+
+  const notesLab = document.createElement("p");
+  notesLab.innerHTML = "Notes from " + plane.user + ": ";
+  dataColumn.append(notesLab);
+
+  const notes = document.createElement("p");
+  if (plane.notes == ""){
+    notes.innerHTML = "User left no notes.";
   }
-  
-  const openModal = (plane) => {
-    const modal = document.getElementById("myModal");
-    const modalContent = document.getElementById("modal-content");
-    
-    modalContent.innerHTML = "";
-  
-    const closeBtn = document.getElementsByClassName("close")[0];
-    closeBtn.onclick = () => modal.style.display = "none";
-  
-    modal.style.display = "block";
-  
-    const columnsContainer = document.createElement("div");
-    columnsContainer.classList.add("columns");
-  
-    const dataColumn = document.createElement("div");
-    dataColumn.classList.add("one");
-  
-    const flightParagraph = document.createElement("p");
-      flightParagraph.innerHTML = "Flight Data:";
-      plane.flight.forEach(flightSegment => {
-          const flightSegmentLine = document.createElement("p");
-          flightSegmentLine.innerHTML = flightSegment;
-          flightParagraph.appendChild(flightSegmentLine);
-      });
-      dataColumn.appendChild(flightParagraph);
-  
-    const date = document.createElement("p");
-    date.innerHTML = "Seen at RDU on " + plane.date + " by " + plane.user;
-    dataColumn.append(date);
-  
-    const imageColumn = document.createElement("div");
-    imageColumn.classList.add("one");
-  
-    const image = document.createElement("img");
-    image.src = "./images/" + plane.main_image;
-    image.style.width = "90%";
-    imageColumn.append(image);
-  
-    columnsContainer.append(dataColumn);
-    columnsContainer.append(imageColumn);
-  
-    modalContent.append(columnsContainer);
+  else{
+  notes.innerHTML = plane.notes;
+  }
+  dataColumn.append(notes);
+
+  const imageColumn = document.createElement("div");
+  imageColumn.classList.add("one");
+
+  const image = document.createElement("img");
+  image.src = "./images/" + plane.img;
+  image.style.width = "90%";
+  imageColumn.append(image);
+
+  planeDetails.append(imageColumn);
+  planeDetails.append(dataColumn);
+
+  eLink.onclick = showPlaneForm;
+  dLink.onclick = () => openWarn(plane);
+
+  populateEditForm(plane);
+
+};
+
+const populateEditForm = (plane)=>{
+  const form = document.getElementById("add-plane-form");
+  form._id.value = plane._id;
+  form.airline.value = plane.airline;
+  form.aircraft.value = plane.aircraft;
+  form.alliance.value = plane.alliance;
+  form.registration.value = plane.registration;
+  form.user.value = plane.user;
+  form.date.value = plane.date;
+  form.notes.value = plane.notes;
+  document.getElementById("img-prev").src = "./images/" + plane.img;
+};
+
+const openDialog = (id) => {
+  document.getElementById("dialog").style.display = "block";
+  document.querySelectorAll("#dialog-details > *").forEach((item)=> {
+      item.classList.add("hidden");
+  });
+  document.getElementById(id).classList.remove("hidden");
+}
+
+const openWarn = (plane) => {
+  document.getElementById("warn").style.display = "block";
+  document.getElementById("dialog").style.display = "none";
+  const btn = document.getElementById("del-final");
+  btn.onclick = () => deletePlane(plane);
+}
+
+const showPlaneForm = (e) => {
+  openDialog("add-plane-form");
+  console.log(e.target);
+  if (e.target.getAttribute("id") != "edit-link") {
+    resetForm();
+  }
+};
+
+const resetForm = () => {
+  const form = document.getElementById("add-plane-form");
+  form.reset();
+  form._id.value = "";
+  document.getElementById("img-prev").src="";
+};
+
+const addEditPlane = async (e) => {
+  e.preventDefault();
+  const form = document.getElementById("add-plane-form");
+  const formData = new FormData(form);
+  let response;
+
+  console.log(...formData);
+
+  //add request
+  if (form._id.value.trim() == "") {
+    console.log("in post");
+    response = await fetch("/api/planes", {
+      method: "POST",
+      body: formData,
+    });
+  } else {
+    console.log("in put");
+    response = await fetch(`/api/planes/${form._id.value}`, {
+      method: "PUT",
+      body: formData,
+    });
   }
 
+  //successfully got data from server
+  if (response.status != 200) {
+    console.log("Error adding / editing data");
+  }
+
+  await response.json();
+  resetForm();
+  document.getElementById("dialog").style.display = "none";
+  showPlanes();
+};
+
+const deletePlane = async(plane) =>{
+  document.getElementById('warn').style.display='none';
+  let response = await fetch(`/api/planes/${plane._id}`,{
+    method:"DELETE",
+    headers:{
+      "Content-Type":"application/json;charset=utf-8",
+    },
+  });
+
+  if(response.status!= 200){
+    console.log("Error deleting");
+    return;
+  }
+
+  let result = await response.json();
+  resetForm();
+  showPlanes();
+  document.getElementById("dialog").style.display = "none";
+};
+
+//on Load
+
 showPlanes();
+document.getElementById("add-plane-form").onsubmit = addEditPlane;
+document.getElementById("add-link").onclick = showPlaneForm;
+
+document.getElementById("img").onchange = (e) => {
+  const prev = document.getElementById("img-prev");
+
+  //they didn't pick an image
+  if(!e.target.files.length){
+      prev.src = "";
+      return;
+  }
+
+  prev.src = URL.createObjectURL(e.target.files.item(0));
+}
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ app.use("/uploads", express.static("uploads"));
 app.use(express.json());
 const cors = require("cors");
 app.use(cors());
+const mongoose = require("mongoose");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -19,108 +20,44 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-let planes = [
-  {
-      "_id": "1",
-      "airline": "jetBlue Airways",
-      "aircraft": "Airbus A320-200",
-      "alliance": "None",
-      "registration": "N624JB",
-      "user": "Admin",
-      "date": "2/12/24",
-      "flight": ["Origin: Raleigh/Durham, NC (RDU)", "Destination: Fort Lauderdale, FL (FLL)", "Duration: 2 hours 8 minutes"],
-      "main_image": "n624jb.jpeg"
-  },
-  {
-      "_id": "2",
-      "airline": "United Airlines",
-      "aircraft": "Airbus A320-200",
-      "alliance": "Star Alliance",
-      "registration": "N446UA",
-      "user": "Admin",
-      "date": "2/11/24",
-      "flight": ["Origin: Chicago, IL (ORD)", "Desination: Raleigh/Durham, NC (RDU)", "Duration: 2 hours 15 minutes"],
-      "main_image": "n446ua.jpeg"
-  },
-  {
-      "_id": "3",
-      "airline": "American Airlines",
-      "aircraft": "Embraer E175-LR",
-      "alliance": "OneWorld",
-      "registration": "N111HQ",
-      "user": "Admin",
-      "date": "2/11/24",
-      "flight": ["Origin: Raleigh/Durham, NC (RDU)", "Destination: New York NY (JFK)", "Duration: 1 hour 31 minutes"],
-      "main_image": "n111hq.jpeg"
-  },
-  {
-      "_id": "4",
-      "airline": "American Airlines",
-      "aircraft": "Boeing 777-200",
-      "alliance": "OneWorld",
-      "registration": "N762AN",
-      "user": "Admin",
-      "date": "2/11/24",
-      "flight": ["Origin: London, England (LHR)", "Destination: Raleigh/Durham, NC (RDU)", "Duration: 8 hours 42 minutes"],
-      "main_image": "n762an.jpeg"
-  },
-  {
-      "_id": "5",
-      "airline": "Breeze Airways",
-      "aircraft": "Airbus A220-300",
-      "alliance": "None",
-      "registration": "N218BZ",
-      "user": "Admin",
-      "date": "2/11/24",
-      "flight": ["Origin: Raleigh/Durham, NC (RDU)", "Destination: Providence, RI (PVD)", "Duration: 1 hour 53 minutes"],
-      "main_image": "n218bz.jpeg"
-  },
-  {
-      "_id": "6",
-      "airline": "Delta Air Lines",
-      "aircraft": "Boeing 757-200",
-      "alliance": "SkyTeam",
-      "registration": "N821DX",
-      "user": "Admin",
-      "date": "3/14/24",
-      "flight": ["Origin: Atlanta, GA (ATL)", "Destination: Raleigh/Durham, NC (RDU)", "Duration: 1 hour 30 minutes"],
-      "main_image": "n821dx.jpeg"
-  },
-  {
-      "_id": "7",
-      "airline": "Southwest Airlines",
-      "aircraft": "Boeing 737-700",
-      "alliance": "None",
-      "registration": "N926WN",
-      "user": "Admin",
-      "date": "3/14/24",
-      "flight": ["Origin: Dallas, TX (DAL)", "Destination: Raleigh/Durham, NC (RDU)", "Duration: 2 hours 32 minutes"],
-      "main_image": "n926wn.jpeg"
-  },
-  {
-      "_id": "8",
-      "airline": "Air Canada Express",
-      "aircraft": "Mitsubishi CRJ-900LR",
-      "alliance": "Star Alliance",
-      "registration": "C-GDJZ",
-      "user": "Admin",
-      "date": "3/14/24",
-      "flight": ["Origin: Raleigh/Durham, NC (RDU)", "Destination: Toronto, Canada (YYZ)", "Duration: 1 hour 42 minutes"],
-      "main_image": "cgdjz.jpeg"
-  }
-]
+mongoose
+    .connect("mongodb+srv://howardty52:fcB1YOLUuSu0qilj@242assignments.vl2gcsf.mongodb.net/")
+    .then(()=> {
+        console.log("connected to mongodb");
+    })
+    .catch((error) => {
+        console.log("couldn't connect to mongodb", error);
+    });
+
+const planeSchema = new mongoose.Schema({
+    airline: String,
+    aircraft: String,
+    alliance: String,
+    registration: String,
+    user: String,
+    date: String,
+    notes: String,
+    img: String
+});
+
+const Plane = mongoose.model("Plane", planeSchema);
 
 app.get("/", (req, res)=>{
     res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/api/planes", (req, res)=>{
+app.get("/api/planes", async (req, res)=>{
+    const planes = await Plane.find();
     res.send(planes);
 });
 
-let latestPlaneId = planes.length > 0 ? planes[planes.length - 1]._id : 0;
+app.get("/api/planes/:id", async (req, res) => {
+    const id = req.params.id;
+    const plane = await Plane.findOne({_id:id});
+    res.send(plane);
+});
 
-app.post("/api/planes", upload.single("img"), (req, res) => {
+app.post("/api/planes", upload.single("img"), async (req, res) => {
   const result = validatePlane(req.body);
 
   if (result.error) {
@@ -128,65 +65,67 @@ app.post("/api/planes", upload.single("img"), (req, res) => {
     return;
   }
 
-  latestPlaneId++;
-
-  const plane = {
-    _id: latestPlaneId,
-    name: req.body.name,
-    description: req.body.description,
-    supplies: req.body.supplies.split(","),
-  };
+  const plane = new Plane ({
+    airline: req.body.airline,
+    aircraft: req.body.aircraft,
+    alliance: req.body.alliance,
+    registration: req.body.registration,
+    user: req.body.user,
+    date: req.body.date,
+    notes: req.body.notes
+  });
 
   if (req.file) {
     plane.img = req.file.filename;
   }
 
-  planes.push(plane);
-  res.send(planes);
+  const saveResult = await plane.save();
+  res.send(plane);
 });
 
-
-app.put("/api/planes/:id", upload.single("img"), (req, res)=>{
-    const plane = planes.find((r)=>r._id === parseInt(req.params.id));
-  
-    if(!planes) res.status(400).send("Plane with given id was not found");
-  
+app.put("/api/planes/:id", upload.single("img"), async (req, res) => {
     const result = validatePlane(req.body);
-  
+
     if(result.error){
-      res.status(400).send(result.error.details[0].message);
-      return;
+        res.status(400).send(result.error.details[0].message);
+        return;
     }
-  
-    plane.name = req.body.name;
-    plane.description = req.body.description;
-    plane.supplies = req.body.supplies.split(",");
-  
-    if(req.file) {
-      plane.img = req.file.filename;
+
+    let toUpdate = {
+      airline: req.body.airline,
+      aircraft: req.body.aircraft,
+      alliance: req.body.alliance,
+      registration: req.body.registration,
+      user: req.body.user,
+      date: req.body.date,
+      notes: req.body.notes
+    };
+
+    if(req.file){
+        toUpdate.img = req.file.filename;
     }
-  
+
+    const id = req. params.id;
+
+    const updateResult = await Plane.updateOne({_id:id},toUpdate);
+    res.send(updateResult);
+});
+
+app.delete("/api/planes/:id", async (req,res) => {
+    const plane = await Plane.findByIdAndDelete(req.params.id);
     res.send(plane);
-  });
-  
-  app.delete("/api/planes/:id", (req, res)=>{
-    const plane = planes.find((r)=>r._id === parseInt(req.params.id));
-  
-    if(!plane){
-      res.status(404).send("The plane with the given id was not found");
-    }
-  
-    const index = planes.indexOf(plane);
-    planes.splice(index,1);
-    res.send(plane);
-  });
+});
 
 const validatePlane = (plane) => {
   const schema = Joi.object({
     _id:Joi.allow(""),
-    supplies:Joi.allow(""),
-    name:Joi.string().min(3).required(),
-    description:Joi.string().min(3).required()
+    airline:Joi.string().min(3).required(),
+    aircraft:Joi.string().min(3).required(),
+    alliance:Joi.allow("").required(),
+    registration:Joi.string().min(3).required(),
+    user:Joi.string().min(3).required(),
+    date:Joi.allow("").required(),
+    notes:Joi.allow("")
   });
 
   return schema.validate(plane);
